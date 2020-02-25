66be4ffcfc98490d7ac904779e172cfbb9a25d44
mohamedelraghy <elraghy8@gmail.com>
2020-02-25 19:32:24 +0200

    succesfully test express

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 
 
+app.get('/api/test', (req, res) => {
+    res.send('Helo World !');
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
