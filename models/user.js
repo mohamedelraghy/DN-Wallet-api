303dcfd642a367f7e25905b24e30ec9347bb8861
mohamedelraghy <elraghy8@gmail.com>
2020-03-03 23:48:18 +0200

    fix string function in Joi validation

--- models/user.js
+++ models/user.js
@@ -41,9 +41,9 @@ const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
     const schema = {
-        name: Joi.String().min(5).max(50).required(),
-        email: Joi.String().min(5).max(255).required().email(),
-        password: Joi.String().min(8).max(255).required()
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
     };
     return Joi.validate(user, schema)
 }
