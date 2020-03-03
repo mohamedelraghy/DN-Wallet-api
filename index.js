d88fc4813ec80ba59514a110082cd2da1a99716a
mohamedelraghy <elraghy8@gmail.com>
2020-03-03 23:58:55 +0200

    call db connection module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 
 require('./startup/routes')(app);
+require('./startup/db')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
