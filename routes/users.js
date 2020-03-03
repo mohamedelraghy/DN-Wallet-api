c2341dc0a7d469de48fe37fb231dad7153e9f43f
mohamedelraghy <elraghy8@gmail.com>
2020-03-03 21:29:32 +0200

    make user post end Point

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const { User, validate } = require('../models/user');
+
+router.get('/', (req, res) => {
+    const { error } = validate(req.body);
+    console.log(error);
+
+});
+
 module.exports = router;
