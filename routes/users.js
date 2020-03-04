4084ecff8f6398dd4b35681fb8630d8ad4b884f8
mohamedelraghy <elraghy8@gmail.com>
2020-03-04 03:05:23 +0200

    create post end point for users

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,23 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
+
 const { User, validate } = require('../models/user');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
     
-    
+    let user = await User.findOne({ email : req.body.email });
+    if(user) return res.status(400).send(`User Already registered`); 
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password', 'gender', 'phone', 'job']));
+    const salt = bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+
 });
 
 module.exports = router;
