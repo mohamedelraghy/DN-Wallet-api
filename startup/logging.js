d82c781efaad005a87659ca82c96acbf085dd7cc
mohamedelraghy <elraghy8@gmail.com>
2020-03-13 01:27:20 +0200

    transport must be an object

--- startup/logging.js
+++ startup/logging.js
@@ -2,15 +2,18 @@ const winston = require('winston');
 //require('winston-mongodb');
 // require('express-async-errors');
 
-module.exports = function() {
-    winston.handleExceptions(
-        new winston.transports.Console({colorize: true, prettyPrint: true}),
-        new winston.transports.File({filename : 'uncaughtExceptions.log'})
-    );
+module.exports = function () {
+    winston.exceptions.handle(
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
+        new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 
-    process.on('unhandledRejection', ex => {
+    process.on('unhandledRejection', (ex) => {
         throw ex;
     });
 
-    winston.add(winston.transports.File, {filename : 'logfile.log'});   
+    winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+    // winston.add(winston.transports.MongoDB, { 
+    //   db: 'mongodb://localhost/vidly',
+    //   level: 'info'
+    // });  
 }
