ebfd6f09a022c35e4f4c60f60805946d49a5f457
mohamedelraghy <elraghy8@gmail.com>
2020-03-13 00:08:52 +0200

    add loggin with winston

---
+++ startup/logging.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+//require('winston-mongodb');
+// require('express-async-errors');
+
+module.exports = function() {
+    winston.handleExceptions(
+        new winston.transports.Console({colorize: true, prettyPrint: true}),
+        new winston.transports.File({filename : 'uncaughtExceptions.log'})
+    );
+
+    process.on('unhandledRejection', ex => {
+        throw ex;
+    });
+
+    winston.add(winston.transports.File, {filename : 'logfile.log'});   
+}
