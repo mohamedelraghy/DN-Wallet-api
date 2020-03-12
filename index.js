122894c9496596b4f441d5463f2320087d34086b
mohamedelraghy <elraghy8@gmail.com>
2020-03-12 22:13:32 +0200

    addin express.static middleware to routes startup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.use('/uploads', express.static('uploads'));
+
 
 require('./startup/routes')(app);
 require('./startup/db')();
--- startup/routes.js
+++ startup/routes.js
@@ -4,5 +4,6 @@ const users = require('../routes/users');
 
 module.exports = function(app){
     app.use(express.json());
+    app.use('/uploads', express.static('uploads'));
     app.use('/api/users', users);
 }
