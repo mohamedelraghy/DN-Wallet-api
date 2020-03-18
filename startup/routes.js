5e67f91f54a8d067ea87d18e2e8687241f7b02ad
mohamedelraghy <elraghy8@gmail.com>
2020-03-18 14:05:17 +0200

    middlware for auth

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('../routes/users');
+const auth = require('../routes/auth');
 
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/uploads', express.static('uploads'));
     app.use('/api/users', users);
+    app.use('/api/auth', auth);
 }
