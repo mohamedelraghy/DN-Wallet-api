3f6a8845b4018eaa1c4c49f66bac02af5ff4f53b
mohamedelraghy <elraghy8@gmail.com>
2020-03-19 13:01:01 +0200

    run config function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
