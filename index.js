e5aff7aab7a50828a599f0902cc2fa6f16532012
mohamedelraghy <elraghy8@gmail.com>
2020-03-19 15:32:24 +0200

    preparing the project for production

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
