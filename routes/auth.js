72dd81760abe6e26c26bb46cad05492dec749431
mohamedelraghy <elraghy8@gmail.com>
2020-03-19 14:02:41 +0200

    Create login user endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
+const { User } = require('../models/user');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    let user = await User.findOne({email:req.body.email});
+    if(!user) return res.status(400).send('Invalid email or password.');
 
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send(error.details[0].message);
+     
+    const token = jwt.sign({_id: user._id}, config.get('jwtKey'));
+    res.send(token);
 });
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    };
+
+    return Joi.validate(req, schema);
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -45,7 +45,7 @@ router.get('/me', async (req, res) => {
 router.post('/', upload.single('photo'), async (req, res) => {
     
     const { error } = validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
     
     let user = await User.findOne({ email : req.body.email });
     if(user) return res.status(400).send(`User Already registered`); 
