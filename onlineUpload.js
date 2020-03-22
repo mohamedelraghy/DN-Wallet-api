05a6d0b11acff9572bc1d12c9e103911c5f0d9c4
mohamedelraghy <elraghy8@gmail.com>
2020-03-22 17:01:38 +0200

    resolve cloudinary config valuses from ENV VAR

--- onlineUpload.js
+++ onlineUpload.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary');
 const config = require('config');
 
 cloudinary.config({
-    cloud_name: 'ddsxppshz',
-    api_key: '446749512425323',
-    api_secret: 'ude_kBk0KUG1tDcBHh9S6koHtF8'
+    cloud_name: config.get('cloud_name'),
+    api_key: config.get('api_key'),
+    api_secret: config.get('api_secret')
 });
 
 
