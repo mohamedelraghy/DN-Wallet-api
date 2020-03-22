8352c13d71e24bb82786afe2fa2f054b3e6a72f4
mohamedelraghy <elraghy8@gmail.com>
2020-03-22 16:10:31 +0200

    set cloudinary config

---
+++ onlineUpload.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary');
+const config = require('config');
+
+cloudinary.config({
+    cloud_name: 'ddsxppshz',
+    api_key: '446749512425323',
+    api_secret: 'ude_kBk0KUG1tDcBHh9S6koHtF8'
+});
+
+
+
