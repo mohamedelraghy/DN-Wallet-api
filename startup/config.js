2ac9ebc7fe27cbebabbaf8e844151304c8f58c56
mohamedelraghy <elraghy8@gmail.com>
2020-03-22 17:14:49 +0200

    Make sure that ENV VAR are set

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,8 @@
 const config = require('config');
 
 module.exports = function(){
-    if(!config.get('jwtKey')){
-        throw new Error('FATAL ERROR: jwtKey is not defined.');
-    }
+    if(!config.get('jwtKey'))throw new Error('FATAL ERROR: jwtKey is not defined.');
+    if (!config.get('cloud_name')) throw new Error('FATAL ERROR: cloud_name is not defined');
+    if (!config.get('api_key')) throw new Error('FATAL ERROR: api_key is not define.');
+    if (!config.get('api_secret')) throw new Error('FATAL ERROR: api_secret is not define.');
 }
