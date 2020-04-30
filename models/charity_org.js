e50078e06321b4e7ef22a49736f178faf9f1a440
mohamedelraghy <elraghy8@gmail.com>
2020-04-30 14:10:40 +0200

    buliding charity model

---
+++ models/charity_org.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const charitySchema = new mongoose.Schema({
+    org_logo : {
+        type : String
+    },
+    org_image: {
+        type : String
+    },
+    address : {
+        type : String,
+        maxlength: 50
+    },
+    founders : {
+        type : String
+    },
+    vision : {
+        type : String
+    },
+    about: {
+        type : String
+    },
+    email : {
+        type : String
+    },
+    phone : {
+        type : String,
+        maxlength: 11
+    },
+    donation_number : {
+        type : Number
+    },
+    amount : {
+        type : Number
+    }
+});
+
+const Charity = mongoose.model('Charity', charitySchema);
+
+function validateCharity(charity){
+    const schema = {
+        org_logo : Joi.string(),
+        org_image : Joi.string(),
+        address : Joi.string().max(50),
+        founders: Joi.string(),
+        vision : Joi.string(),
+        about : Joi.string(),
+        email : Joi.string().email(),
+        phone : Joi.string().max(11),
+        donation_number: Joi.number(),
+        amount : Joi.number() 
+    }
+    return Joi.validate(charity, schema);
+}
+
+exports.Charity = Charity;
+exports.validate = validateCharity;
