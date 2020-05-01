96086c53e00c8a8b60a2f044f2ea17d86eabc5c9
mohamedelraghy <elraghy8@gmail.com>
2020-05-01 16:21:39 +0200

    POST for creating charity

--- routes/charity_orgs.js
+++ routes/charity_orgs.js
@@ -1,5 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const { Charity, validate } = require('../models/charity_org');
+const _ = require('lodash');
+const imgUpload = require('../middleware/multer');
+const cloudinary = require('../onlineUpload');
 
+
+router.get('/', async (req, res) => {
+    const charity = await Charity.find();
+    res.json(charity);
+});
+
+router.post('/create', imgUpload, async (req, res) =>{
+
+    const { error } = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let charity = new Charity(_.pick(req.body, ['name', 'address', 'founders', 'vision', 'about', 'email', 'phone']));
+
+    if (req.files[0]) {
+        const picAttr = await cloudinary.uploads(req.files[0].path);
+        charity.org_logo = picAttr.url;
+        fs.unlinkSync(req.files[0].path);
+    }
+    if (req.files[1]) {
+        const picAttr = await cloudinary.uploads(req.files[1].path);
+        charity.org_image = picAttr.url;
+        fs.unlinkSync(req.files[1].path);
+    }
+
+    await charity.save()
+    res.json(charity)
+
+});
 
 module.exports = router;
