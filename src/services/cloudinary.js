9eb5b318a0670ef27e1bdeba33c7531999fd839e
mohamedelraghy <elraghy8@gmail.com>
2020-05-05 17:25:51 +0200

    suplay service with it's configration

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,4 +1,12 @@
 const cloudinary = require('cloudinary').v2;
+const config = require('config');
+
+cloudinary.config({
+  cloud_name: config.get('cloud_name'),
+  api_key: config.get('api_key'),
+  api_secret: config.get('api_secret')
+});
+
 
 async function cloudinaryUpload(path) {
   return await cloudinary.uploader.upload(path, {
