3805d087447c7b3efd0a19e7e3e69454c88b609f
mohamedelraghy <elraghy8@gmail.com>
2020-05-05 17:28:17 +0200

    get logined user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,14 @@
 
 const express = require('express');
 const router = express.Router();
-const { User, validate } = require('../models/user');
+
 const auth = require('../middleware/auth');
 
 const usersController = require('../controllers/users');
 
 
-router.get('/me', auth, async (req, res) => {
-    const user = await User.findById(req.user._id).select('-password');
-    res.send(user);
-    
-});
-// TypeScript
+
+router.get('/me', auth, usersController.Me);
 router.post('/register', usersController.register);
 
 // router.put('/info',)
