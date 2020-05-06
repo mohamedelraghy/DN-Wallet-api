32ec516f2374ba0daccc67b6ccd2246fc13d16cf
mohamedelraghy <elraghy8@gmail.com>
2020-05-06 20:12:45 +0200

    create donate function

---
+++ src/controllers/charity/donate.js
@@ -0,0 +1,20 @@
+const { Charity } = require('../../models/charity_org');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+async function donate(req, res) {
+
+    const id = req.params.id;
+    if(!ObjectId.isValid(id)) return res.status(400).json('Not valid ID');
+
+    let chariy = await Charity.findById(id);
+    if(!chariy) return res.status(400).json('Charity with the given ID not found');
+
+    
+    chariy.donation_number += 1;
+    chariy.amount += req.body.amount;
+
+    chariy.save();
+    res.status(200).json(chariy);
+}
+
+module.exports = donate;
