372d1841a4b69da8e032ce031868bd8149ae5d1b
mohamedelraghy <elraghy8@gmail.com>
2020-05-07 13:44:57 +0200

    call contacts route

--- src/routes/userContacts.js
+++ src/routes/userContacts.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
+const contactsController = require('../controllers/contacts');
+
+router.get('/', (req, res) => {
+    
+});
+
+router.post('/create/:id', auth, contactsController.Create);
 
 module.exports = router;
