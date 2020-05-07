53e7dbbc5d78e4ce231052acf803dc4742cda401
mohamedelraghy <elraghy8@gmail.com>
2020-05-07 17:09:57 +0200

    save conatct in DB

--- src/controllers/contacts/createContact.js
+++ src/controllers/contacts/createContact.js
@@ -10,12 +10,19 @@ async function create(req, res){
     const user = await User.findById(contactID);
     if(!user) res.status(400).json('User With The Given ID is not Found');
 
+    console.log(req.user._id);
     let contact = new Contact({
-        user : req.user.id,
-        constacts : [contactID]
+        user : req.user._id
     });
 
+    const newConatct = {
+        userID : contactID
+    }
+
+    contact.constacts.unshift(newConatct);
+
     await contact.save();
+    res.status(200).json(contact)
 }
 
 module.exports = create;
