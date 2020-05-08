769018b03453550d84179e58a894a6efb2fab1d3
mohamedelraghy <elraghy8@gmail.com>
2020-05-08 15:13:33 +0200

    populate user

--- src/controllers/contacts/showAll.js
+++ src/controllers/contacts/showAll.js
@@ -3,7 +3,7 @@ const { Contact } = require('../../models/contacts');
 async function showAll(req, res){
     const userID = req.user._id;
     const contacts = await Contact.find({ user: userID })
-        .populate('user contacts.userID', '-_id -password -photo');
+        .populate('user contacts.userID', ' -password -photo');
 
     if(!contacts) return res.status(400).json('No contacts Found');
     res.status(200).send(contacts);
