9b142673fcfca261c80b86c51baa1606f9d169af
mohamedelraghy <elraghy8@gmail.com>
2020-05-08 15:13:01 +0200

    make sure that contact is not already added or add yourseld as a contact

--- src/controllers/contacts/createContact.js
+++ src/controllers/contacts/createContact.js
@@ -8,12 +8,19 @@ async function create(req, res){
     if (!ObjectId.isValid(contactID)) return res.status(400).json('Invaild ID');
     
     const user = await User.findById(contactID);
-    if(!user) res.status(400).json('User With The Given ID is not Found');
+    if(!user) return res.status(400).json('User With The Given ID is not Found');
 
-    console.log(req.user._id);
-    let contact = new Contact({
-        user : req.user._id
-    });
+    let contact = await Contact.findOne({user: req.user._id});
+    if(!contact) {
+        contact = new Contact({
+            user: req.user._id
+        });
+    }
+    
+    const found = contact.contacts.find(contact => contact.userID == contactID); // cast to objectID 
+    if(found) return res.status(400).json('Contact Already Exists');
+    
+    if(req.user._id == contactID) return res.status(400).json('You cannot Add yourself As a contact');
 
     const newConatct = {
         userID : contactID
