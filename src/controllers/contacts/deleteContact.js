9bb5b609a0f64d348559d48b41a0f198650e2452
mohamedelraghy <elraghy8@gmail.com>
2020-05-09 23:45:36 +0200

    function to delete contact

--- src/controllers/contacts/deleteContact.js
+++ src/controllers/contacts/deleteContact.js
@@ -1,5 +1,23 @@
+const { Contact } = require('../../models/contacts');
+const { User } = require('../../models/user')
+const ObjectId = require('mongoose').Types.ObjectId;
+
 async function deleteContact(req, res){
+    const removedContactID = req.params.id;
+    if (!ObjectId.isValid(removedContactID)) return res.status(400).json('Invaild ID');
+
+    const user = await User.findById(removedContactID);
+    if (!user) return res.status(400).json('User With The Given ID is not Found');
+
+    let contact = await Contact.findOne({ user: req.user._id });
     
+    const removedContactIndex = contact.contacts.findIndex(contact => contact.userID == removedContactID);
+    if(removedContactIndex === -1) return res.status(400).json('No Such a Contact')
+    const removedContact = contact.contacts.splice(removedContactIndex, 1);
+
+    await contact.save();
+
+    res.status(200).json(removedContact)
 }
 
 module.exports = deleteContact;
