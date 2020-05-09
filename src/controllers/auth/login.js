d2f344009ab772f89d089301957356a6775a18a5
mohamedelraghy <elraghy8@gmail.com>
2020-05-09 21:24:15 +0200

    move auth function to auth controller

---
+++ src/controllers/auth/login.js
@@ -0,0 +1,29 @@
+const { User } = require('../../models/user');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
+
+
+async function login (req, res) {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send(error.details[0].message);
+
+    const token = user.generateAuthToken();
+    res.json({ "token": token });
+}
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    };
+
+    return Joi.validate(req, schema);
+}
+
+module.exports = login;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,30 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('../models/user');
-const Joi = require('joi');
-const bcrypt = require('bcryptjs');
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    
-    let user = await User.findOne({email:req.body.email});
-    if(!user) return res.status(400).send('Invalid email or password.');
+const authController = require('../controllers/auth');
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send(error.details[0].message);
-     
-    const token = user.generateAuthToken();
-    res.json({"token" : token});
-});
-
-function validate(req) {
-    const schema = {
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(8).max(255).required()
-    };
-
-    return Joi.validate(req, schema);
-}
+router.post('/', authController.login);
 
 module.exports = router;
