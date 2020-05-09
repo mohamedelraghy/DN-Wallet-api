7b8282048add589f0c3e2fc8172e2fdb185cc5cf
mohamedelraghy <elraghy8@gmail.com>
2020-05-09 21:34:48 +0200

    move function implementaion to its controller

--- src/routes/charity_orgs.js
+++ src/routes/charity_orgs.js
@@ -4,11 +4,7 @@ const router = express.Router();
 
 const charityController = require('../controllers/charity')
 
-
-router.get('/', async (req, res) => {
-    const charity = await Charity.find();
-    res.json(charity);
-});
+router.get('/', charityController.charityList);
 
 router.post('/create', charityController.Create);
 
