364e266eefd67472c0b3f4bcdcceeedd47a00344
mohamedelraghy <elraghy8@gmail.com>
2020-05-09 21:34:02 +0200

    export show all charity module

--- src/controllers/charity/index.js
+++ src/controllers/charity/index.js
@@ -1,7 +1,10 @@
 const Create = require('./create');
 const donate = require('./donate');
+const charityList = require('./showAll');
 
 module.exports = {
     Create,
     donate,
+    charityList,
+    
 }
