9320b66b0627a442ce2c26cedecc120f1a6cdfff
mohamedelraghy <elraghy8@gmail.com>
2020-05-19 15:46:34 +0200

    bulid send verification code

--- src/controllers/verfiy/sendCode.js
+++ src/controllers/verfiy/sendCode.js
@@ -1,17 +1,24 @@
 const config = require('config');
-const client = require("twilio")(config.get('twilio-account_sid'), config.get('twilio-auth_token'));
+const twilio = require("twilio")(config.get('twilio-account_sid'), config.get('twilio-auth_token'));
 
 async function sendCode(req, res){
 
-    const send = await client
-                            .verify
-                            .services(config.get('twilio-service_id'))
+    const phoneNumber = req.body.phoneNumber;
+    if(phoneNumber[0] !== '+') return res.status(400).json('You should send country code');
+
+    try{
+        const verificationRequest = await twilio.verify.services(config.get('twilio-service_id'))
+                            .verifications
                             .create({
-                                to : req.phoneNumber,
+                                to : req.body.phoneNumber,
                                 channel : 'sms'
                             })
+    } catch(err) {
+        return res.status(400).json({"error": err})
+    }
 
-    res.json(send)
+    
+    return res.status(200).json(verificationRequest.status)
 }
 
 module.exports = sendCode;
