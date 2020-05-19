4e18ce82a4c12d1c42aa9639304ffb4e4b00fc61
mohamedelraghy <elraghy8@gmail.com>
2020-05-19 15:00:52 +0200

    bulid sedn verfication code function

--- src/controllers/verfiy/sendCode.js
+++ src/controllers/verfiy/sendCode.js
@@ -1,3 +1,17 @@
 const config = require('config');
 const client = require("twilio")(config.get('twilio-account_sid'), config.get('twilio-auth_token'));
 
+async function sendCode(req, res){
+
+    const send = await client
+                            .verify
+                            .services(config.get('twilio-service_id'))
+                            .create({
+                                to : req.phoneNumber,
+                                channel : 'sms'
+                            })
+
+    res.json(send)
+}
+
+module.exports = sendCode;
