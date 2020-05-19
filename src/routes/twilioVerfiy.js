9285e16a2f0ae2a0bd93be6a9f82c6de4c409ba7
mohamedelraghy <elraghy8@gmail.com>
2020-05-19 15:22:14 +0200

    add get route to send verification code

--- src/routes/twilioVerfiy.js
+++ src/routes/twilioVerfiy.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const verificationController = require('../controllers/verfiy')
+
+router.get('/', verificationController.sendCode);
 
 module.exports = router;
