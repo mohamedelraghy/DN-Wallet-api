13f4793f4c1516cc9a37a6ef8dbe22971f71dfbe
mohamedelraghy <elraghy8@gmail.com>
2020-05-20 04:15:02 +0200

    verify code that have been send

---
+++ src/controllers/verfiy/codeCheck.js
@@ -0,0 +1,21 @@
+const config = require('config');
+const twilio = require("twilio")(config.get('twilio-account_sid'), config.get('twilio-auth_token'));
+
+async function codeCheck(req, res){
+    const code = req.body.code;
+    let verificationResult;
+
+    try{
+        verificationResult = await twilio.verify.services(config.get('twilio-service_id'))
+                                        .verificationChecks
+                                        .create({
+                                            code ,
+                                            to : req.body.phoneNumber
+                                        });
+    }catch(err) {
+        return res.status(400).json({ "error": err })
+    }
+
+    return res.status(200).json({ "Verified": verificationRequest.status })
+
+}
