98153b96f30ed1f2a495d449218b69bd09e406b7
mohamedelraghy <elraghy8@gmail.com>
2020-05-27 04:02:38 +0200

    execlude _id when showing it

--- src/controllers/charity/charityDetails.js
+++ src/controllers/charity/charityDetails.js
@@ -1,7 +1,7 @@
 const { Charity } = require('../../models/charity_org');
 
 async function charityDetails(req, res) {
-    const charity = await Charity.findById(req.params.id);
+    const charity = await Charity.findById(req.params.id).select('-_id');
     res.json(charity);
 }
 
