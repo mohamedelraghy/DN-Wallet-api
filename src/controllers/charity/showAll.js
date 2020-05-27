7007e7dd7bd432f03a0d8582d4b1cb57fece0abb
mohamedelraghy <elraghy8@gmail.com>
2020-05-27 03:22:36 +0200

    select a specific data in show all chariy

--- src/controllers/charity/showAll.js
+++ src/controllers/charity/showAll.js
@@ -1,7 +1,7 @@
 const { Charity } = require('../../models/charity_org');
 
 async function showAll (req, res) {
-    const charity = await Charity.find();
+    const charity = await Charity.find().select('name email org_logo');
     res.json(charity);
 }
 
