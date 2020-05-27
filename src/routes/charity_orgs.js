2aeaf588317cbb77bf018e8f0685e568d31e959d
mohamedelraghy <elraghy8@gmail.com>
2020-05-27 03:40:30 +0200

    add route to show charity info

--- src/routes/charity_orgs.js
+++ src/routes/charity_orgs.js
@@ -6,6 +6,8 @@ const charityController = require('../controllers/charity')
 
 router.get('/', charityController.charityList);
 
+router.get('/:id', charityController.charityDetails);
+
 router.post('/create', charityController.Create);
 
 router.post('/donate/:id', charityController.donate);
