a2b37346574be36f40293e6e403a6a3fc50888f4
mohamedelraghy <elraghy8@gmail.com>
2020-05-29 01:53:45 +0200

    make suer that user is login

--- src/routes/twilioVerfiy.js
+++ src/routes/twilioVerfiy.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const verificationController = require('../controllers/verfiy')
 
-router.get('/', verificationController.sendCode);
-router.post('/', verificationController.codeCheck);
+router.get('/', auth, verificationController.sendCode);
+router.post('/', auth, verificationController.codeCheck);
 
 module.exports = router;
