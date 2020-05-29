c5e8ef0b4257410d5a4d7a9604f90c2574d7eb06
mohamedelraghy <elraghy8@gmail.com>
2020-05-29 14:15:16 +0200

    dont show user id

--- src/controllers/users/me.js
+++ src/controllers/users/me.js
@@ -2,7 +2,7 @@ const { User } = require('../../models/user');
 
 
 async function Me (req, res){
-    const user = await User.findById(req.user._id).select('-password');
+    const user = await User.findById(req.user._id).select('-_id -password');
     res.send(user);
 }
 
