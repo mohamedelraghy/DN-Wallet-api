d84b814bcdcc499fe8e4b4d34a603c91d7562fa4
mohamedelraghy <elraghy8@gmail.com>
2020-05-29 21:34:33 +0200

    update user info

--- src/controllers/users/editUser.js
+++ src/controllers/users/editUser.js
@@ -1,10 +1,17 @@
 const { User } = require('../../models/user');
 
 async function editUser(req, res) {
-    const user = await User.findById(req.user._id).select('-_id -password');
+    let user = await User.findByIdAndUpdate({_id: req.user._id}, {
+        $set: {
+            country: req.body.country,
+            gender: req.body.gender,
+            job: req.body.job 
+        }
+    }, {new: true});
 
     if(!user) return res.status(400).json('User not Found');
 
+    console.log(req.body);
     console.log(user);
 }
 
