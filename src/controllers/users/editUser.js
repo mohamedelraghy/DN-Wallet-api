5cf3bd71add6685b683b7bd80534338a64bc59f5
mohamedelraghy <elraghy8@gmail.com>
2020-05-29 20:32:07 +0200

    bulid the big pic for editUser function

--- src/controllers/users/editUser.js
+++ src/controllers/users/editUser.js
@@ -1,7 +1,11 @@
 const { User } = require('../../models/user');
 
 async function editUser(req, res) {
+    const user = await User.findById(req.user._id).select('-_id -password');
 
+    if(!user) return res.status(400).json('User not Found');
+
+    console.log(user);
 }
 
 module.exports = editUser;
