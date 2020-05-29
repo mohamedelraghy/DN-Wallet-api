6e34d7759ee4ff5ec5db7a9360f1e034f2c552fb
mohamedelraghy <elraghy8@gmail.com>
2020-05-29 14:20:57 +0200

    user not found

--- src/controllers/users/me.js
+++ src/controllers/users/me.js
@@ -3,6 +3,7 @@ const { User } = require('../../models/user');
 
 async function Me (req, res){
     const user = await User.findById(req.user._id).select('-_id -password');
+    if(!user) return res.status(400).json('user not found');
     res.send(user);
 }
 
