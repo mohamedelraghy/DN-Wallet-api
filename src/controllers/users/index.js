c472abf50365eaf7f1f414945d5a5df289882f13
mohamedelraghy <elraghy8@gmail.com>
2020-05-30 17:26:41 +0200

    export delete picture module

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -2,10 +2,12 @@ const register = require('./register');
 const Me = require('./me');
 const editUser = require('./editUser');
 const profilePic = require('./profilePic');
+const deletePic = require('./DeletPic');
 
 module.exports = {
   register,
   Me,
   editUser,
   profilePic,
+  deletePic,
 }
