f3d301f8fb7b28e452d171b6c256745543f677c2
mohamedelraghy <elraghy8@gmail.com>
2020-05-30 14:07:45 +0200

    fuction to set profile picture

---
+++ src/controllers/users/profilePic.js
@@ -0,0 +1,30 @@
+const { User } = require('../../models/user');
+const cloudinaryUpload = require('../../services/cloudinary');
+const formidable = require('formidable')
+
+async function setProfilePic(req, res) {
+    const form = new formidable.IncomingForm();
+    form.parse(req, async(formError, fields, files) => {
+        if(formError){
+            console.error(form);
+            return res.status(400).json(formError);
+        }
+        
+        req.body = files;
+
+        let user = await User.findById(req.user._id);
+        if(!user) return res.status(400).json('User not found');
+
+        if(files.photo){
+            const {secure_url, err} = await cloudinaryUpload(files.photo.path);
+            if(err) {
+                console.error(err);
+                res.status(400).json(err);
+            }
+
+            user.photo = secure_url;
+        }
+    });
+}
+
+module.exports = setProfilePic;
