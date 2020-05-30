c10c1c3197ca03ca19d500818e7c17ad9acadc5e
mohamedelraghy <elraghy8@gmail.com>
2020-05-30 17:31:49 +0200

    function to delete picture

--- src/controllers/users/DeletPic.js
+++ src/controllers/users/DeletPic.js
@@ -2,6 +2,16 @@ const { User } = require('../../models/user');
 
 async function deletePic(req, res){
 
+    const user = await User.findById(req.user._id);
+    if(!user) return res.status(200).json('User not Found');
+
+    user.photo = '';
+
+    await user.save();
+
+    console.log(user);
+    return res.status(200).json('profile Picture Deleted');
+
 }
 
 module.exports = deletePic;
