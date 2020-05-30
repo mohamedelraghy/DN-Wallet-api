19aae39f127e025d0ef04bd04ad05adf259e519a
mohamedelraghy <elraghy8@gmail.com>
2020-05-30 17:14:00 +0200

    add some comments to make it easy beezee

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,12 +6,12 @@ const auth = require('../middleware/auth');
 const usersController = require('../controllers/users');
 
 
-router.post('/register', usersController.register);
+router.post('/register', usersController.register); // create new user
 
-router.get('/me', auth, usersController.Me);
+router.get('/me', auth, usersController.Me); // show user info
 
-router.put('/info', auth, usersController.editUser);
+router.put('/info', auth, usersController.editUser); // edit user info
 
-router.post('/pic', auth, usersController.profilePic);
+router.post('/pic', auth, usersController.profilePic); // add profile picture
 
 module.exports = router;
