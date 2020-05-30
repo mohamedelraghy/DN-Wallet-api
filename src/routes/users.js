de92a896353f9af8670b635ce718a3d1faecfd4f
mohamedelraghy <elraghy8@gmail.com>
2020-05-30 17:11:58 +0200

    add picture route

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,11 +6,12 @@ const auth = require('../middleware/auth');
 const usersController = require('../controllers/users');
 
 
-router.get('/me', auth, usersController.Me);
-
 router.post('/register', usersController.register);
+
+router.get('/me', auth, usersController.Me);
 
 router.put('/info', auth, usersController.editUser);
 
+router.post('/pic', auth, usersController.profilePic);
 
 module.exports = router;
