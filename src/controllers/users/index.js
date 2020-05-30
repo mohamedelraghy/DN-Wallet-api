a98ab005f9c510797968f2441283bc346b68ff36
mohamedelraghy <elraghy8@gmail.com>
2020-05-30 14:08:14 +0200

    export profile pic fucntion

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,9 +1,11 @@
 const register = require('./register');
 const Me = require('./me');
 const editUser = require('./editUser');
+const profilePic = require('./profilePic');
 
 module.exports = {
   register,
   Me,
   editUser,
+  profilePic,
 }
