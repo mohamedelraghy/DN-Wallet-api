a5a30ee43c1468e1c29995ce40d3c00e1884b710
mohamedelraghy <elraghy8@gmail.com>
2020-05-31 13:03:45 +0200

    enhance editUser funciton

--- src/controllers/users/editUser.js
+++ src/controllers/users/editUser.js
@@ -4,19 +4,11 @@ async function editUser(req, res) {
     let user = await User.findById(req.user._id);
     if(!user) return res.status(400).json('User not Found');
 
-    const country = user.country;
-    const gender = user.gender;
-    const job = user.job;
 
-    if(!req.body.country) user.country = country;
-    else user.country = req.body.country;
-
-    if(!req.body.gender) user.gender = gender;
-    else user.gender = req.body.gender;
-
-    if(!req.body.job) user.job = job;
-    else user.job = req.body.job;
-
+    for (const key in req.body) {
+        user[key] = req.body[key];
+    }
+   
     await user.save();
     
     return res.status(200).json('User info edit successfully');
