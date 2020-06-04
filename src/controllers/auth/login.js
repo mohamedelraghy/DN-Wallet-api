4d58347a20bc061235bbdf50a34ee6d4549b995a
mohamedelraghy <elraghy8@gmail.com>
2020-06-04 05:20:19 +0200

    edit response

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -8,10 +8,10 @@ async function login (req, res) {
     if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send('Invalid email or password.');
+    if (!user) return res.status(400).json({"error": "Invalid email or password."});
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send(error.details[0].message);
+    if (!validPassword) return res.status(400).json({"error": "Invalid email or password."});
 
     const token = user.generateAuthToken();
     res.json({ "token": token });
