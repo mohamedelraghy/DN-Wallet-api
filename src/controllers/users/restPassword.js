587a986e08f5190b9c489d7b77635e09c021252b
mohamedelraghy <elraghy8@gmail.com>
2020-06-04 05:22:31 +0200

    function to rest to new password

--- src/controllers/users/restPassword.js
+++ src/controllers/users/restPassword.js
@@ -1,6 +1,23 @@
-const jwt = require('jsonwebtoken');
+const { User } = require('../../models/user');
+const bcrypt = require('bcryptjs');
+
 
 async function restPassword(req, res) {
+
+    const token = req.params.token;
+    const user = await User.findOne({ restToken: token, restTokenExpiration: { $gt: Date.now()}});
+
+    if(!user) return res.status(400).json({"error" : "Invalid Token"});
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(req.body.password, salt);
+
+    user.restToken = undefined;
+    user.restTokenExpiration = undefined;
+
+    await user.save();
+
+    res.status(200).json({"Message" : "password Reseted Successfully"});
 
 }
 
