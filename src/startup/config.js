3dc060768e5465bd1df4d695a43c68838b5b1726
mohamedelraghy <elraghy8@gmail.com>
2020-06-04 03:06:26 +0200

    make sure that send grid api key value are provided

--- src/startup/config.js
+++ src/startup/config.js
@@ -7,5 +7,6 @@ module.exports = function(){
     if (!config.get('api_secret')) throw new Error('FATAL ERROR: api_secret is not define.');
     if (!config.get('twilio-service_id')) throw new Error('FATAL ERROR: Twilio SERVICE ID is not define.');
     if (!config.get('twilio-account_sid')) throw new Error('FATAL ERROR: Twilio ACCOUNT SID is not define.');
-    if (!config.get('twilio-auth_token')) throw new Error('FATAL ERROR: Twilio AUTH TOKEN is not defined.');
+    if (!config.get('twilio-auth_token')) throw new Error('FATAL ERROR: Twilio AUTH TOKEN is not define.');
+    if (!config.get('sendGrid-api_key')) throw new Error('FATAL ERROR: SendGrid API_key is not define.')
 }
