ccc5efddeaacff4357d4e97b173fa75e7ed0edac
mohamedelraghy <elraghy8@gmail.com>
2020-06-11 14:23:39 +0200

    edit response of get User data request

--- src/controllers/users/me.js
+++ src/controllers/users/me.js
@@ -3,8 +3,8 @@ const { User } = require('../../models/user');
 
 async function Me (req, res){
     const user = await User.findById(req.user._id).select('-_id -password');
-    if(!user) return res.status(400).json('user not found');
-    res.send(user);
+    if(!user) return res.status(400).json({ "error": "user not found"});
+    res.status(200).json(user);
 }
 
 module.exports = Me;
