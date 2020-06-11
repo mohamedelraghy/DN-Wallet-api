72b612d10fc2e0bee144e2151b4b2d7431d178d5
mohamedelraghy <elraghy8@gmail.com>
2020-06-11 13:44:21 +0200

    return 200 status when we send all charity

--- src/controllers/charity/showAll.js
+++ src/controllers/charity/showAll.js
@@ -2,7 +2,7 @@ const { Charity } = require('../../models/charity_org');
 
 async function showAll (req, res) {
     const charity = await Charity.find().select('name email org_logo');
-    res.json(charity);
+    res.status(200).json(charity);
 }
 
 module.exports = showAll;
