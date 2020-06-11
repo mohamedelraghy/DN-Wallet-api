683ec7545298a3d5712b4b0c576a5cece064baa8
mohamedelraghy <elraghy8@gmail.com>
2020-06-11 03:17:32 +0200

    edit respone of delete cnotacts request

--- src/controllers/contacts/deleteContact.js
+++ src/controllers/contacts/deleteContact.js
@@ -4,10 +4,10 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 async function deleteContact(req, res){
     const removedContactID = req.params.id;
-    if (!ObjectId.isValid(removedContactID)) return res.status(400).json('Invaild ID');
+    if (!ObjectId.isValid(removedContactID)) return res.status(400).json({"error": "Invaild ID"});
 
     const user = await User.findById(removedContactID);
-    if (!user) return res.status(400).json('User With The Given ID is not Found');
+    if (!user) return res.status(400).json({ "error": "User With The Given ID is not Found"});
 
     let contact = await Contact.findOne({ user: req.user._id });
     
@@ -17,7 +17,7 @@ async function deleteContact(req, res){
 
     await contact.save();
 
-    res.status(200).json(removedContact)
+    res.status(200).json({ "error" : null })
 }
 
 module.exports = deleteContact;
