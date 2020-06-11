6556edb62df13cf63f3cd2c93e5fb659c23340e4
mohamedelraghy <elraghy8@gmail.com>
2020-06-11 13:43:02 +0200

    edit response of donate request

--- src/controllers/charity/donate.js
+++ src/controllers/charity/donate.js
@@ -4,12 +4,11 @@ const ObjectId = require('mongoose').Types.ObjectId;
 async function donate(req, res) {
 
     const id = req.params.id;
-    if(!ObjectId.isValid(id)) return res.status(400).json('InValid ID');
+    if(!ObjectId.isValid(id)) return res.status(400).json({ "error": "InValid ID" });
 
     let chariy = await Charity.findById(id);
-    if(!chariy) return res.status(400).json('Charity with the given ID not found');
+    if(!chariy) return res.status(400).json({ "error": "Charity with the given ID not found" });
 
-    
     chariy.donation_number += 1;
     chariy.amount += req.body.amount;
 
