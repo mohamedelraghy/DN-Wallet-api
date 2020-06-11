27396b87297174a8568dc952450b02b3b0170ff4
mohamedelraghy <elraghy8@gmail.com>
2020-06-11 13:34:56 +0200

    edit response of login request

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -5,13 +5,13 @@ const bcrypt = require('bcryptjs');
 
 async function login (req, res) {
     const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).json({ "error": error.details[0].message });
 
     let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).json({"error": "Invalid email or password."});
+    if (!user) return res.status(400).json({ "error": "Invalid email or password." });
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).json({"error": "Invalid email or password."});
+    if (!validPassword) return res.status(400).json({ "error": "Invalid email or password." });
 
     const token = user.generateAuthToken();
     res.json({ "token": token });
