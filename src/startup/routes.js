9008ac1c4f758d8c23e49633419c621a924e1a6a
mohamedelraghy <elraghy8@gmail.com>
2020-06-14 15:16:09 +0200

    update cors function

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -8,7 +8,7 @@ const verfiy = require('../routes/twilioVerfiy');
 module.exports = function(app){
     app.use(express.json());
     app.use('/uploads', express.static('uploads'));
-    app.use(function(req, res, next) {
+    app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
