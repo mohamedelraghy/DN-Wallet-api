80ae318b45a4eb595000fc8a7552a8458a88d725
mohamedelraghy <elraghy8@gmail.com>
2020-06-14 12:39:53 +0200

    fix cros origin resoruce problem

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -4,11 +4,16 @@ const auth = require('../routes/auth');
 const charity = require('../routes/charity_orgs');
 const contact = require('../routes/userContacts');
 const verfiy = require('../routes/twilioVerfiy');
-
+const config = require('config');
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/uploads', express.static('uploads'));
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", config.get('Domain'));
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.use('/api/users', users);
     app.use('/api/auth', auth);
     app.use('/api/charity', charity);
