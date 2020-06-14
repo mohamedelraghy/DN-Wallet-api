ffdb2891edbff2d7b36460ec4ff439dd6ca0a61e
mohamedelraghy <elraghy8@gmail.com>
2020-06-14 14:10:28 +0200

    change the value of Origin

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -10,7 +10,7 @@ module.exports = function(app){
     app.use(express.json());
     app.use('/uploads', express.static('uploads'));
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", config.get('Domain'));
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
