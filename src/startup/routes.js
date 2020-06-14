861432ae88c60bd7421f4ff9918b34bc91cb175a
mohamedelraghy <elraghy8@gmail.com>
2020-06-14 14:07:38 +0200

    set methods for cors

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -11,6 +11,7 @@ module.exports = function(app){
     app.use('/uploads', express.static('uploads'));
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", config.get('Domain'));
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
