60e912e482534892823963ef68225d220539e688
mohamedelraghy <elraghy8@gmail.com>
2020-06-14 16:29:57 +0200

    change the value of origin

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -9,7 +9,7 @@ module.exports = function(app){
     app.use(express.json());
     app.use('/uploads', express.static('uploads'));
     app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "herokuapp.com");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
