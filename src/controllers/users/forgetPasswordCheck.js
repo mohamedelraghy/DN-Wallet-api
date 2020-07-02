82b6fbb98aa878a6a746e01db182491f64e9591a
mohamedelraghy <elraghy8@gmail.com>
2020-07-02 17:56:58 +0200

    validate user input

--- src/controllers/users/forgetPasswordCheck.js
+++ src/controllers/users/forgetPasswordCheck.js
@@ -4,7 +4,10 @@ const Joi = require('joi');
 
 
 async function checkCode(req, res){
-   
+
+    const { error } = validate(req.body);
+    if (error) return res.status(400).json({ "error": error.details[0].message });
+
     const user = await User.findOne({ email: req.body.email, resetCode: req.body.code, resetCodeExpiration: { $gt: Date.now() } });
     if (!user) return res.status(400).json({ "error": "Invalid Code" });
 
@@ -21,7 +24,7 @@ function validate(req){
         email : Joi.string().email().required(),
         code : Joi.string().min(4).max(4)
     }
-    
+
     return Joi.validate(req, schema);
 }
 
