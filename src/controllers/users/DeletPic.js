28d0e3631a64c9ffa9a0a7cb2462bb06de599ac0
mohamedelraghy <elraghy8@gmail.com>
2020-07-05 23:41:23 +0200

    change response delete photo request

--- src/controllers/users/DeletPic.js
+++ src/controllers/users/DeletPic.js
@@ -9,7 +9,7 @@ async function deletePic(req, res){
 
     await user.save();
 
-    return res.status(200).json();
+    return res.status(200).json({ "success": "Photo deleted succussfully"} );
 }
 
 module.exports = deletePic;
