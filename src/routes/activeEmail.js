b689d5ef2d8ec14fedd6587befef76e7aeb73416
mohamedelraghy <elraghy8@gmail.com>
2020-07-06 02:41:16 +0200

    add get route to send active key to user

--- src/routes/activeEmail.js
+++ src/routes/activeEmail.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router  = express.Router();
 
+const auth = require('../middleware/auth');
 
+const verficationController = require('../controllers/verfiy');
+
+router.get('/', auth, verficationController.emailcode); // send activation key to email
 
 module.exports = router;
