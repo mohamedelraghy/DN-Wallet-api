38972d74c36505f074fd4e966e59873246d10162
mohamedelraghy <elraghy8@gmail.com>
2020-07-06 03:09:23 +0200

    create check active key function

--- src/controllers/verfiy/emailCodeCheck.js
+++ src/controllers/verfiy/emailCodeCheck.js
@@ -1,9 +1,30 @@
 const { User } = require('../../models/user');
+const Joi = require('joi');
 
 async function checkActiveKey(req, res){
-    
+
+    const { error } = validate(req.body);
+    if(error) return res.status(400).json({ "error": error.details[0].message} );
+
+    const user = await User.findOne({ _id: req.user._id, emailCode: req.body.code, emailCodeExpiration: { $gt: Date.now() } }).select('-password');
+    if (!user) return res.status(400).json({ "error": "Invalid Code" });
+
+    user.accountIsActive = true;
+    user.emailCode = undefined;
+    user.emailCodeExpiration = undefined;
+
+    await user.save();
+
+    return res.status(200).json({ "success" : "Account is activated", "accountIsActuve": user.accountIsActive });
 
 }
 
+function validate(req) {
+    const schema = {
+        code: Joi.string().min(4).max(4).required()
+    }
+
+    return Joi.validate(req, schema);
+}
 
 module.exports = checkActiveKey;
