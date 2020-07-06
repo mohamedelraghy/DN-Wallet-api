2ebf2e06fe3854745cc5eb38bf6b67147598aa0b
mohamedelraghy <elraghy8@gmail.com>
2020-07-06 02:45:46 +0200

    bulid a big pic for check Active key route

---
+++ src/controllers/verfiy/emailCodeCheck.js
@@ -0,0 +1,8 @@
+const { User } = require('../../models/user');
+
+async function checkActiveKey(req, res){
+
+}
+
+
+module.exports = checkActiveKey;
--- src/controllers/verfiy/index.js
+++ src/controllers/verfiy/index.js
@@ -1,9 +1,11 @@
 const sendCode = require('./sendCode');
 const codeCheck = require('./codeCheck');
 const emailcode = require('./emailCode')
+const checkActiveKey = require('./emailCodeCheck');
 
 module.exports = {
     sendCode,
     codeCheck,
     emailcode,
+    checkActiveKey
 }
