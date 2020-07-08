20cbab85c38c669f6bc441d7e0d8d918e10d21f0
mohamedelraghy <elraghy8@gmail.com>
2020-07-09 00:26:34 +0200

    set the default pic when delete user one

--- src/controllers/users/DeletPic.js
+++ src/controllers/users/DeletPic.js
@@ -1,11 +1,12 @@
 const { User } = require('../../models/user');
+const config = require('config');
 
 async function deletePic(req, res){
 
     const user = await User.findById(req.user._id);
     if(!user) return res.status(400).json({ "error": "User not Found" });
 
-    user.photo = '';
+    user.photo = config.set('profilePic');
 
     await user.save();
 
