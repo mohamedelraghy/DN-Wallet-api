fada0cff12631a3e88c3be0b89ad6e1f6be20a7e
mohamedelraghy <elraghy8@gmail.com>
2020-07-09 00:31:03 +0200

    set some dumy data for testsing

--- src/controllers/users/me.js
+++ src/controllers/users/me.js
@@ -2,9 +2,34 @@ const { User } = require('../../models/user');
 
 
 async function Me (req, res){
-    const user = await User.findById(req.user._id).select('-_id -password');
+    const user = await User.findById(req.user._id).select('-password');
     if(!user) return res.status(400).json({ "error": "user not found" });
-    res.status(200).json(user);
+    
+    const balance = [ // amout : double , curr : code 
+        {
+            id: "asdlhgkfjasdgvkjjpo134421",
+            amount : 500.50,
+            currency : "EGP"
+        },
+        {
+            id: "asdlhgkfjasdgvkjjpo134428",
+            amount: 20.50,
+            currency: "USD"
+        }
+    ]
+
+    const crad = [
+        {
+            id : "asdlhgkfjasdgvkjjpo134423",
+            name : "Visa",
+            type : "perpaid",
+            last4digits : "7894"
+        }
+    ]
+
+    res.status(200).json({"user": user, "balance" : balance, "payment_cards" : crad });
+
+
 }
 
 module.exports = Me;
