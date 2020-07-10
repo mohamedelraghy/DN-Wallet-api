b51bf51b7f9b86529916b0c783c90f30df7ef388
mohamedelraghy <elraghy8@gmail.com>
2020-07-11 00:36:12 +0200

    crate pay function

--- src/controllers/pay/pay.js
+++ src/controllers/pay/pay.js
@@ -1,5 +1,19 @@
+const config = require('config');
+const stripe = require("stream")(config.get("stripeKey"));
 
 async function pay(req, res) {
+
+    stripe.charges.create(
+      {
+        amount: 2000,
+        currency: "usd",
+        source: "tok_visa",
+        description: "My First Test Charge (created for API docs)",
+      },
+      function (err, charge) {
+        console.log(charge);  
+      }
+    );
 
 }
 
