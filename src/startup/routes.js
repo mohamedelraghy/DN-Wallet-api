77fc39fc8974b071efeefc75efdcc9a905c98411
mohamedelraghy <elraghy8@gmail.com>
2020-07-10 17:34:14 +0200

    set Access-Control-Allow-header to x-auth-token

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -12,7 +12,7 @@ module.exports = function(app){
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
         next();
     });
     app.use('/api/users', users);
