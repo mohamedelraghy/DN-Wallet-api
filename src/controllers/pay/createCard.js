f8870ebe8396b1908acd15d5b091b4716d3c29c7
mohamedelraghy <elraghy8@gmail.com>
2020-07-20 02:51:11 +0200

    create card

--- src/controllers/pay/createCard.js
+++ src/controllers/pay/createCard.js
@@ -3,6 +3,8 @@ const config = require('config');
 const stripe = require('stripe')(config.get('stripeKey'));
 
 async function createCard(req, res) {
+    const customer = await User.findById(req.user._id).select('stripeID');
+    
 
 }
 
