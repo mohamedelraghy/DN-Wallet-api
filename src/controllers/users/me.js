8a32e44cb777128d60f21783e479b06c51d382d7
mohamedelraghy <elraghy8@gmail.com>
2020-07-29 00:17:08 +0200

    deleting some dumy data

--- src/controllers/users/me.js
+++ src/controllers/users/me.js
@@ -5,29 +5,9 @@ async function Me (req, res){
     const user = await User.findById(req.user._id).select('-password');
     if(!user) return res.status(400).json({ "error": "user not found" });
     
-    const balance = [ // amout : double , curr : code 
-        {
-            id: "asdlhgkfjasdgvkjjpo134421",
-            amount : 500.50,
-            currency : "EGP"
-        },
-        {
-            id: "asdlhgkfjasdgvkjjpo134428",
-            amount: 20.50,
-            currency: "USD"
-        }
-    ]
+    
 
-    const crad = [
-        {
-            id : "asdlhgkfjasdgvkjjpo134423",
-            name : "Visa",
-            type : "perpaid",
-            last4digits : "7894"
-        }
-    ]
-
-    res.status(200).json({"user": user, "balance" : balance, "payment_cards" : crad });
+    res.status(200).json({"user": user });
 
 
 }
