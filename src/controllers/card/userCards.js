2a449a906e93c4409b96e529778ccf71f34d2bce
mohamedelraghy <elraghy8@gmail.com>
2020-07-29 20:42:02 +0200

    correct mis-written for a stuats function

--- src/controllers/card/userCards.js
+++ src/controllers/card/userCards.js
@@ -5,7 +5,7 @@ const { Card } = require('../../models/card');
 async function getUserCards(req, res) {
 
     const cards = await Card.find({ cardHolder : req.user._id });
-    res.satas(200).json(cards);
+    return res.status(200).json(cards);
 }
 
 module.exports = getUserCards;
