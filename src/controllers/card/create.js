60839a1f4acfb96b95c6a6c55d3a55dd6b879bdd
mohamedelraghy <elraghy8@gmail.com>
2020-07-30 00:57:45 +0200

    create a card

--- src/controllers/card/create.js
+++ src/controllers/card/create.js
@@ -1,7 +1,20 @@
-const { Card } = require('../../models/card');
+const { Card, Validate } = require('../../models/card');
+const _ = require('lodash');
 
 
 async function createCard(req, res){
+
+    const { error } = Validate(req.body);
+    if (error) return res.status(400).json({ "error": error.details[0].message });
+
+    let card = await Card.findOne({cardNumber : req.body.cardNumber });
+    if(card) return res.status(400).json({ "error" : "failed to create card" });
+
+    card = new Card(_.pick(req.body, ['cardNumber', 'expMonth', 'expYear', 'cvc', 'cardType']));
+    card.cardHolder = req.user._id;
+    card.balance = 0;
+    
+    await card.save();
 
 }
 
