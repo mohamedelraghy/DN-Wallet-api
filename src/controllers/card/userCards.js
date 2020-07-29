50788d862b799a4ff321e67842a2767b172871f2
mohamedelraghy <elraghy8@gmail.com>
2020-07-29 20:50:07 +0200

    return bad request if is not a response

--- src/controllers/card/userCards.js
+++ src/controllers/card/userCards.js
@@ -5,6 +5,8 @@ const { Card } = require('../../models/card');
 async function getUserCards(req, res) {
 
     const cards = await Card.find({ cardHolder : req.user._id });
+    if(cards.length == 0) return res.status(400).json({ "error": "User don't have a card" });
+
     return res.status(200).json(cards);
 }
 
