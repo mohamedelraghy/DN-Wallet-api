14dab3e726a884628cf5881bc4cd5cdc4a24d1b6
mohamedelraghy <elraghy8@gmail.com>
2020-07-29 19:54:59 +0200

    call cards endpoint

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -1,5 +1,12 @@
 const  express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const cardController = require('../controllers/card');
+
+router.get('/', auth, cardController.cards);
+
+router.post('/', auth, cardController.createCard);
+
 
 module.exports = router;
