3531eb028e16092c2d9549998d3d80ca4e78a736
mohamedelraghy <elraghy8@gmail.com>
2020-08-01 15:19:14 +0200

    create bank

--- src/controllers/bank/create.js
+++ src/controllers/bank/create.js
@@ -6,10 +6,18 @@ async function createBank(req, res) {
     const { error } = Validate(req.body);
     if (error) return res.status(400).json({ "error": error.details[0].message });
 
-    let bank = await Bank.findOne({ bankName : req.body.bankName });
+    let bank = await Bank.findOne({ name : req.body.name });
     if(bank) return res.status(400).json({ "error": "cannot create a bank" });
 
-    bank = new Bank(_.pick(req.body, ['amount', 'currency_code']));
+    bank = new Bank(_.pick(req.body, ['name']));
+
+    console.log(bank);
+    const balance = {
+        amount: req.body.amount,
+        currency_code: req.body.currency_code
+    }
+
+    bank.balance.unshift(balance);
 
     await bank.save();
 
