d5d7365c3a69222d40696397fb218cd3878b10c9
mohamedelraghy <elraghy8@gmail.com>
2020-08-01 18:58:00 +0200

    make sure amount i s postive number

--- src/controllers/card/charge.js
+++ src/controllers/card/charge.js
@@ -5,7 +5,14 @@ const { Bank } = require('../../models/bank');
 async function charage(req, res) {
 
     const card = await Card.findOne({ cardHolder : req.user._id});
-    console.log(card);
+    const bank = await Bank.findOne({ name : "DN-Wallet" });
+    
+    if(!card) return res.status(400).json({ "error" : "User have no Card" });
+    
+    if(req.body.amount <= 0) return res.status(400).json({ "error" : "amount can't be 0 or negative number"});
+
+    
+
 }
 
 module.exports = charage;
