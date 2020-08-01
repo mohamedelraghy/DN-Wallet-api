79129a272c36e4e9600736560b0c8fe943dc02f7
mohamedelraghy <elraghy8@gmail.com>
2020-08-01 14:04:01 +0200

    create validation function for bank

--- src/models/bank.js
+++ src/models/bank.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const bankSchema = new mongoose.Schema({
-    balace : [{ type : Number }];
+    balace : [{
+        amount : Number,
+        currency_code : {
+            type : String,
+            enum: ['EGP', 'USD', 'EUR', 'JPY', 'SAR', null],
+        }
+    }]
 });
 
 const Bank = mongoose.model('Bank', bankSchema);
+
+function validateBank(bank){
+    const schema = {
+        amount : Joi.number().required(),
+        currency_code: Joi.string().valid('EGP', 'USD', 'EUR', 'JPY', 'SAR')
+    }
+}
 
 exports.Bank = Bank;
