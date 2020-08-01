712c23989e7579343ee384c683f5b5f21df020d1
mohamedelraghy <elraghy8@gmail.com>
2020-08-01 18:38:41 +0200

    export charge card

--- src/controllers/card/index.js
+++ src/controllers/card/index.js
@@ -1,8 +1,10 @@
 const createCard = require('./create');
 const cards  = require('./userCards');
+const charge = require('./charge');
 
 
 module.exports = {
     createCard,
     cards,
+    charge
 }
