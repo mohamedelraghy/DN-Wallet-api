1e5d6dd046dd403a069c98cfa642471c3161e77b
mohamedelraghy <elraghy8@gmail.com>
2020-08-01 18:39:20 +0200

    function to charge card

--- src/controllers/card/charge.js
+++ src/controllers/card/charge.js
@@ -1,1 +1,11 @@
-d
+const { Card } = require('../../models/card');
+const { Bank } = require('../../models/bank');
+
+
+async function charage(req, res) {
+
+    const card = await Card.findOne({ cardHolder : req.user._id});
+    console.log(card);
+}
+
+module.exports = charage;
