32f919549d47126bde0032f3a80833fa2b00af02
mohamedelraghy <elraghy8@gmail.com>
2020-08-01 19:44:45 +0200

    convert balance t obj

--- src/controllers/card/create.js
+++ src/controllers/card/create.js
@@ -12,7 +12,6 @@ async function createCard(req, res){
 
     card = new Card(_.pick(req.body, ['cardNumber', 'expMonth', 'expYear', 'cvc', 'cardType']));
     card.cardHolder = req.user._id;
-    card.balance = 0;
     
     await card.save();
 
