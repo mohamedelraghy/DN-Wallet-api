0317cf8007022cd9b4de713d75714458f73051c2
mohamedelraghy <elraghy8@gmail.com>
2020-08-02 17:09:38 +0200

    function to create card to user and charity

--- src/controllers/card/create.js
+++ src/controllers/card/create.js
@@ -5,39 +5,40 @@ const ObjectId = require('mongoose').Types.ObjectId;
 const _ = require('lodash');
 
 
-async function createCard(req, res){
+async function createCard(req, res) {
 
     const { error } = Validate(req.body);
     if (error) return res.status(400).json({ "error": error.details[0].message });
 
-    let card = await Card.findOne({cardNumber : req.body.cardNumber });
-    if(card) return res.status(400).json({ "error" : "failed to create card" });
+    let card = await Card.findOne({ cardNumber: req.body.cardNumber });
+    if (card) return res.status(400).json({ "error": "failed to create card" });
 
     card = new Card(_.pick(req.body, ['cardNumber', 'expMonth', 'expYear', 'cvc', 'cardType']));
-    
+
     const charityID = req.params.charityID;
     if (charityID) {
-        if (!ObjectId.isValid(transferTo)) return res.status(400).json({ "error": "Invaild ID" });
+
+        if (!ObjectId.isValid(charityID)) return res.status(400).json({ "error": "Invaild ID" });
 
         const charity = await Charity.findById(charityID);
-        if(!charity) return res.status(400).json({ "error" : "No charity with the given ID" });
-        
-        charity.cardID = card._id;
-        
-        await card.save();
-    }else {
+        if (!charity) return res.status(400).json({ "error": "No charity with the given ID" });
+
+        charity.cardID.unshift(card._id);
+
+        await charity.save();
+    
+    } else {
 
         const user = await User.findById(req.user._id);
-        if(!user) return res.status(400).json({ "error" : "No user with the given ID" });
-        
-        user.cardID = card._id;
-    
+        if (!user) return res.status(400).json({ "error": "No user with the given ID" });
+
+        user.cardID.unshift(card._id);
+
         await user.save();
     }
     await card.save();
 
     return res.status(200).json(card);
-    
 }
 
 module.exports = createCard;
