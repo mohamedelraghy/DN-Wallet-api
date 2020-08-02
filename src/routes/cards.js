b7db9e88e1021e3a5642d4e082057e5438e93d2a
mohamedelraghy <elraghy8@gmail.com>
2020-08-02 17:45:31 +0200

    add router to get charity card info

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const cardController = require('../controllers/card');
 
-router.get('/', auth, cardController.cards);
+router.get('/', auth, cardController.cards); // card info for user
+
+router.get('/:charityId', auth, cardController.cards); // card info for cahrity 
 
 router.post('/', auth, cardController.createCard); // create card for user
 
