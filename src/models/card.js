811e66e8d737a6eed56223568ad9535a01f5ec55
mohamedelraghy <elraghy8@gmail.com>
2020-08-02 15:15:51 +0200

    remove unnecessary proprtey from card model

--- src/models/card.js
+++ src/models/card.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const cardSchema = new mongoose.Schema({
-    cardHolder : {
-        type: mongoose.Types.ObjectId,
-        ref: 'User'
-    },
     cardNumber : {
         type : String,
         length : 16,
