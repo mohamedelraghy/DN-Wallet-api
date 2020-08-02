2b413174d8abcb7b620525d770ac74d8feeb20eb
mohamedelraghy <elraghy8@gmail.com>
2020-08-02 15:39:44 +0200

    create card for charity

--- src/controllers/card/create.js
+++ src/controllers/card/create.js
@@ -1,4 +1,7 @@
 const { Card, Validate } = require('../../models/card');
+const { User } = require('../../models/user');
+const { Charity } = require('../../models/charity_org');
+const ObjectId = require('mongoose').Types.ObjectId;
 const _ = require('lodash');
 
 
@@ -11,8 +14,21 @@ async function createCard(req, res){
     if(card) return res.status(400).json({ "error" : "failed to create card" });
 
     card = new Card(_.pick(req.body, ['cardNumber', 'expMonth', 'expYear', 'cvc', 'cardType']));
-    card.cardHolder = req.user._id;
     
+    const charityID = req.params.charityID;
+    if (charityID) {
+        if (!ObjectId.isValid(transferTo)) return res.status(400).json({ "error": "Invaild ID" });
+
+        const charity = await Charity.findById(charityID);
+        if(!charity) return res.status(400).json({ "error" : "No charity with the given ID" });
+        
+        charity.cardID = card._id;
+        await card.save();
+    }
+    const user = await User.findById(req.user._id);
+    user.cardID = card._id;
+
+    await user.save();
     await card.save();
 
     return res.status(200).json(card);
