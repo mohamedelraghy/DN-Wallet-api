9f79e72dfc8bcfe2100e0ba0f62c111f40435181
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 18:41:39 +0200

    validate body obj

--- src/controllers/card/Withdrawal.js
+++ src/controllers/card/Withdrawal.js
@@ -1,8 +1,27 @@
 const { User } = require('../../models/user');
+const { Card } = require('../../models/card');
+const ObjectId = require('mongoose').Types.ObjectId;
+const Joi = require('joi');
 
 async function withdraw(req, res) {
+
+    const { error } = validate(req.body);
+    if(erro) res.status(400).json({ "error" : error.details[0].message });
     
     const user = await User.findById(req.user._id);
+    if(!user) res.status(400).json({ "error" : "User not fount" });
+
+
+}
+
+function validate(req) {
+
+    const schema = {
+        amount: Joi.number().required().positive(),
+        currency_code: Joi.string().valid('EGP', 'USD', 'EUR', 'JPY').required()
+    }
+
+    return Joi.validate(req, schema);
 }
 
 module.exports = withdraw;
