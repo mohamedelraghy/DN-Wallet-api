2eb125588aa06a1853c78a553c03fc5f494b2fe6
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 03:19:06 +0200

    get card info for charity

--- src/controllers/card/cardinfo.js
+++ src/controllers/card/cardinfo.js
@@ -1,10 +1,22 @@
 const { User } = require('../../models/user');
-
+const { Charity } = require("../../models/charity_org");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 
 async function getUserCards(req, res) {
 
+    const charityID = req.params.charityId;
     
+    if(charityID){
+        if (!ObjectId.isValid(charityID)) return res.status(400).json({ error: "Invaild ID" });
+
+        const cards = await Charity.findById(charityID)
+            .populate("cards.cardID").select("cards");
+        if (!cards) return res.status(400).json({ error: "No charity with the given ID" });
+
+        return res.status(200).json(cards);
+    }
+
     const cards = await User.findById(req.user._id)
         .populate("cards.cardID").select("cards");
     if(!cards) return res.status(400).json({ "error": "User don't have a card" });
