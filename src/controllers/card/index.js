116ab86145fc8c96d2698a898492f6bf41139662
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 18:15:45 +0200

    import withraw

--- src/controllers/card/index.js
+++ src/controllers/card/index.js
@@ -2,11 +2,13 @@ const createCard = require('./create');
 const cards  = require('./cardinfo');
 const charge = require('./charge');
 const transfer = require('./transfer');
+const withdraw = require('./Withdrawal');
 
 
 module.exports = {
     createCard,
     cards,
     charge,
-    transfer
+    transfer,
+    withdraw
 }
