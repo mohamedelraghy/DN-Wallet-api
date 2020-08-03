bcff86c9758e6070ad0fda550589377f2366caf1
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 18:34:02 +0200

    get logined user

--- src/controllers/card/Withdrawal.js
+++ src/controllers/card/Withdrawal.js
@@ -1,5 +1,8 @@
-async function withdraw(req, res) {
+const { User } = require('../../models/user');
 
+async function withdraw(req, res) {
+    
+    const user = await User.findById(req.user._id);
 }
 
 module.exports = withdraw;
