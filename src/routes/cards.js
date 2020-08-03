78852e493ae12940b15d2d83ea4be9874390c1f5
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 14:48:51 +0200

    make some updates in endpoints

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -8,11 +8,11 @@ router.get('/', auth, cardController.cards); // card info for user
 
 router.get('/:charityId', auth, cardController.cards); // card info for cahrity 
 
-router.post('/', auth, cardController.createCard); // create card for user
+router.post('/create', auth, cardController.createCard); // create card for user
 
-router.post('/:charityID', auth, cardController.createCard); // create card for charity
+router.post('/create/:charityID', auth, cardController.createCard); // create card for charity
 
-router.post('/charge', auth, cardController.charge);
+router.post('/charge/:cardID', auth, cardController.charge);
 
 router.post('/transfer/:id', auth, cardController.transfer);
 
