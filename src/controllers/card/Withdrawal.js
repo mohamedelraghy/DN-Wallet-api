fe1478c22d749a7f752f3625f526910b9b37c9f0
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 19:13:31 +0200

    add amount to card

--- src/controllers/card/Withdrawal.js
+++ src/controllers/card/Withdrawal.js
@@ -6,19 +6,31 @@ const Joi = require('joi');
 async function withdraw(req, res) {
 
     const { error } = validate(req.body);
-    if(erro) res.status(400).json({ "error" : error.details[0].message });
+    if(error) res.status(400).json({ "error" : error.details[0].message });
+
+    const amount = req.body.amount;
+    const currency = req.body.currency_code;
     
     const user = await User.findById(req.user._id).select("cards cryptedAcc publicKey");
     if(!user) res.status(400).json({ "error" : "User not fount" });
 
     const cardID = req.params.cardID;
-    if(!ObjectId.valid(cardID)) return res.status(400).json({"error" : "Invalid ID"});
+    if(!ObjectId.isValid(cardID)) return res.status(400).json({"error" : "Invalid ID"});
 
     const card = await Card.findById(cardID);
     if(!card) return res.status(400).json({ "error" : "card not found" });
 
-    
+    const found = card.balance.find(balance => balance.currency_code == currency);
+    if(!found) {
+        found.amount = amount;
+        found.currency_code = currency;
+    } else {
+        found.amount += amount
+    }
 
+    card.save();
+
+    return res.status(400).json({ "success" : "Transaction Done successfully" });
 }
 
 function validate(req) {
