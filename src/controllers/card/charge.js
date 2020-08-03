9a36dc3acd9bc432baf6243a6a069d4ebb2dba2c
mohamedelraghy <elraghy8@gmail.com>
2020-08-03 14:48:32 +0200

    prepare charge function to work with blockchain

--- src/controllers/card/charge.js
+++ src/controllers/card/charge.js
@@ -1,31 +1,39 @@
-const { Card } = require('../../models/card');
-// const { Bank } = require('../../models/bank');
+const { User } = require('../../models/user');
+const { Card } = require('../../models/card'); 
+const ObjectId = require('mongoose').Types.ObjectId;
 const Joi = require('joi');
 
 
-async function charage(req, res) {
+async function charge(req, res) {
 
+    
     const { error } = validate(req.body);
     if(error) return res.status(400).json({ "error" : error.details[0].message});
 
-    const card = await Card.findOne({ cardHolder : req.user._id});
-    // const bank = await Bank.findOne({ name : "DN-Wallet" });
-    
-    if(!card) return res.status(400).json({ "error" : "User have no Card" });
-    
-    const found = card.balance.find(balance => balance.currency_code == req.body.currency_code);
-    if (!found) {
-        const balance = {
-            amount: req.body.amount,
-            currency_code: req.body.currency_code
-        }
+    const amount = req.body.amount;
+    const currency = req.body.currency_code;
+    const acc = await User.findById(req.user._id)
+        .populate("cards.cardID").select("cards cryptedAcc publicKey");
+   
+    const cardID = req.params.cardID;
+    console.log(cardID);
+    if (!ObjectId.isValid(cardID)) return res.status(400).json({ "error" : "Invalid ID"});
 
-        card.balance.unshift(balance);
-    } else {    
-        found.amount += req.body.amount;
-    }
+    const card = await Card.findById(cardID);
+    
+    // const found = card.balance.find(balance => balance.currency_code == req.body.currency_code);
+    // if (!found) {
+    //     const balance = {
+    //         amount: req.body.amount,
+    //         currency_code: req.body.currency_code
+    //     }
 
-    await card.save();
+    //     card.balance.unshift(balance);
+    // } else {    
+    //     found.amount += req.body.amount;
+    // }
+
+    // await card.save();
 
     
     return res.status(200).json(card);
@@ -34,9 +42,9 @@ async function charage(req, res) {
 function validate(req){
     const schema = {
         amount: Joi.number().required().positive(),
-        currency_code: Joi.string().valid('EGP', 'USD', 'EUR', 'JPY', 'SAR').required()
+        currency_code: Joi.string().valid('EGP', 'USD', 'EUR', 'JPY').required()
     }
     return Joi.validate(req, schema);
 }
 
-module.exports = charage;
+module.exports = charge;
