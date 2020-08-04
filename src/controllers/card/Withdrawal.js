b9ba4b534d06e1ba9deaf3ada7641db371ac89a8
mohamedelraghy <elraghy8@gmail.com>
2020-08-04 15:02:48 +0200

    fix a punch of errors

--- src/controllers/card/Withdrawal.js
+++ src/controllers/card/Withdrawal.js
@@ -4,8 +4,9 @@ const web3 = new Web3('https://rinkeby.infura.io/v3/0c43e1f8e5434b2ab24b6b1bcbad
 const mainAccount = '0xdea47E0C2737A1eCbc7a24D3941EeACBd8B44c3C';
 const contractAdress = '0x5E92b68b8ED963FF0e573104118f99Fc1a91BB3D'
 const abi = [{"constant":false,"inputs":[{"name":"client","type":"address"},{"name":"uSd","type":"uint256"},{"name":"eGp","type":"uint256"},{"name":"eUr","type":"uint256"},{"name":"jPy","type":"uint256"}],"name":"changeCurrencies","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"amount","type":"uint256"}],"name":"transferTo","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getCurrency","outputs":[{"components":[{"name":"USD","type":"uint256"},{"name":"EGP","type":"uint256"},{"name":"EUR","type":"uint256"},{"name":"JPY","type":"uint256"}],"name":"","type":"tuple"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getHistory","outputs":[{"components":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"amount","type":"uint256"}],"name":"","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"walletBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"from","type":"address"},{"indexed":false,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"time","type":"uint256"}],"name":"TransactionCompleted","type":"event"}]
-var dnwalletContract = new web3.eth.Contract(abi,contractAdress)
-
+var dnwalletContract = new web3.eth.Contract(abi,contractAdress);
+const config = require('config');
+const privateKey = Buffer.from(config.get('ethjsPrivateKey'), 'hex')
 
 
 
@@ -36,14 +37,14 @@ async function withdraw(req, res) {
         found.amount = amount;
         found.currency_code = currency;
     } else {
-        found.amount += amount
-        const gas =  withdrawFromAccount(user.cryptedAcc,user.email,amount,currency);
-        updataingCurrenct(user.cryptedAcc,user.email,amount,currency,gas);
+        withdrawFromAccount(user.cryptedAcc,user.email,amount,currency);
+        updataingCurrenct(user.cryptedAcc,user.email,amount,currency, 115704);
+        found.amount += amount;
     }
 
     card.save();
 
-    return res.status(400).json(card);
+    return res.status(200).json(card);
 }
 
 function validate(req) {
@@ -61,7 +62,7 @@ const withdrawFromAccount = async(JSONfile,userEmail,amount,currency) =>
     let etherValue;
     var newChangeCurrency = [0,0,0,0];
     const accountCurrency = await dnwalletContract.methods.getCurrency().call({from:userAccount['address']});
-    const balance;
+    let balance;
     if(currency == 'USD')
     {
         balance = accountCurrency[0];
@@ -85,7 +86,6 @@ const withdrawFromAccount = async(JSONfile,userEmail,amount,currency) =>
     }
     if (amount >= balance)
     {
-      console.log('unable to make transaction');
       return 0
     }
 
@@ -108,10 +108,6 @@ const withdrawFromAccount = async(JSONfile,userEmail,amount,currency) =>
     const serializedTx = tx.serialize();
     const raw = '0x' + serializedTx.toString('hex');
     const txHash = await web3.eth.sendSignedTransaction(raw);
-    console.log(txHash);
-    const transactionInfo = await web3.eth.getTransactionReceipt(txHash);
-    console.log(transactionInfo);
-    return(transactionInfo['gasUsed']);
   }
 
 
@@ -134,8 +130,8 @@ const withdrawFromAccount = async(JSONfile,userEmail,amount,currency) =>
       newChangeCurrency[1] = amount;
       treansactionFees =  treansactionFees * 6256;
       newChangeCurrency[1] = Number(accountHistory['EGP']) - (newChangeCurrency[1] + treansactionFees );
-      newChangeCurrency[1] = newChangeCurrency[1].toFixed(0);
-      console.log(newChangeCurrency[1]);
+      newChangeCurrency[1] = Math.floor(newChangeCurrency[1]);
+     
     }else if(currency == 'EUR')
     {
       newChangeCurrency[2] = amount;
@@ -149,6 +145,7 @@ const withdrawFromAccount = async(JSONfile,userEmail,amount,currency) =>
       newChangeCurrency[3] = Number(accountHistory['JPY']) - (newChangeCurrency[3] + treansactionFees );
       newChangeCurrency[3] = newChangeCurrency[3].toFixed(0);
     }
+    
     const changeCurrencyFunctionData = dnwalletContract.methods.changeCurrencies(userAccount['address'],newChangeCurrency[0],newChangeCurrency[1],newChangeCurrency[2],newChangeCurrency[3]).encodeABI();
     const txCount = await web3.eth.getTransactionCount(mainAccount);
     const txObject = 
@@ -164,6 +161,5 @@ const withdrawFromAccount = async(JSONfile,userEmail,amount,currency) =>
     const serializedTx = tx.serialize();
     const raw = '0x' + serializedTx.toString('hex');
     const txHash = web3.eth.sendSignedTransaction(raw);
-
   }
 module.exports = withdraw;
