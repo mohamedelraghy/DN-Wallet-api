208bce546c8f5417e943cc2365238fc6192bd02f
mohamedelraghy <elraghy8@gmail.com>
2020-08-05 18:43:02 +0200

    prepare charity to be doneated

--- src/controllers/charity/donate.js
+++ src/controllers/charity/donate.js
@@ -6,11 +6,17 @@ async function donate(req, res) {
     const id = req.params.id;
     if(!ObjectId.isValid(id)) return res.status(400).json({ "error": "InValid ID" });
 
-    let chariy = await Charity.findById(id);
+    let chariy = await Charity.findById(id).select("publicKey");
     if(!chariy) return res.status(400).json({ "error": "Charity with the given ID not found" });
 
+    const user = await User.findById(req.user._id).select('cryptedAcc publicKey email');
+    if(!user) return res.status(400).json({ "error" : "user with the given ID is not found" });
+    
+    
+    
+    
     chariy.donation_number += 1;
-    chariy.amount += req.body.amount;
+   
 
     await chariy.save();
     res.status(200).json(chariy);
