289158426c7f73a6e936496ea6cbad5dcf1db37a
mohamedelraghy <elraghy8@gmail.com>
2020-08-08 23:33:16 +0200

    cros

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -15,7 +15,7 @@ module.exports = function(app){
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");    
-        res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, content-type, content-disposition, Accept, x-auth-token, authorization");
+        res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
         next();
     });
     // app.use(cors());
