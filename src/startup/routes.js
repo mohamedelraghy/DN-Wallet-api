28ea77062cd7067ac02425aadcc304872ffdc74b
mohamedelraghy <elraghy8@gmail.com>
2020-08-08 23:03:36 +0200

    use cors

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -12,14 +12,14 @@ const bank = require('../routes/banks');
 module.exports = function(app){
     app.use(express.json());
     app.use('/uploads', express.static('uploads'));
-    // app.use((req, res, next) => {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    //     // Origin, X-Requested-With, Content-Type, Content-Disposition, Accept, x-auth-token, authorization
-    //     res.header("Access-Control-Allow-Headers", "*");
-    //     next();
-    // });
-    app.use(cors());
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        // Origin, X-Requested-With, Content-Type, Content-Disposition, Accept, x-auth-token, authorization
+        res.header("Access-Control-Allow-Headers", "*");
+        next();
+    });
+    // app.use(cors());
     app.use('/api/users', users);
     app.use('/api/auth', auth);
     app.use('/api/charity', charity);
