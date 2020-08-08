be69a37fdd6e25956809c2b36182f2fb12610ee2
mohamedelraghy <elraghy8@gmail.com>
2020-08-08 11:14:24 +0200

    add header to be allowed

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -14,7 +14,7 @@ module.exports = function(app){
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Content-Disposition, Accept, x-auth-token, authorization");
         next();
     });
     app.use('/api/users', users);
