e565a72db2f10354d6e4461ee8499cdd1206a8db
mohamedelraghy <elraghy8@gmail.com>
2020-08-10 15:45:57 +0200

    validate input to add heir function

--- src/controllers/users/addHeir.js
+++ src/controllers/users/addHeir.js
@@ -1,5 +1,19 @@
+const Joi = require('joi');
+
 async function heir(req, res) {
+
+    
 
 }
 
-module.exports = heir;
+function validate(req){
+    
+    const schema = {
+        first_user: Joi.string().min(5).max(255).required().email(),
+        second_user: Joi.string().min(5).max(255).required().email(),
+        precentage : Joi.number().positive().required()
+    }
+    return Joi.validate(req, schema);
+}
+
+module.exports   = heir;
